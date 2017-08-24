0078129297604a25fb463a3341725783f0c09575
Fulup Ar Foll <fulup@iot.bzh>
2017-08-24 14:34:52 +0200

    tart script to match repo renaming

--- start-demo-daemon.sh
+++ start-demo-daemon.sh
@@ -21,12 +21,12 @@
 #MPD_PORT=6601 mpc playlist
 
 # Load default playlists
-#MPD_PORT=6601 mpc clear
-#MPD_PORT=6601 mpc load default
-#MPD_PORT=6602 mpc clear
-#MPD_PORT=6602 mpc load default
-#MPD_PORT=6603 mpc clear
-#MPD_PORT=6603 mpc load default
+# MPD_PORT=6601 mpc clear
+# MPD_PORT=6601 mpc load default
+# MPD_PORT=6602 mpc clear
+# MPD_PORT=6602 mpc load default
+# MPD_PORT=6603 mpc clear
+# MPD_PORT=6603 mpc load default
 
 
 # BUG CONTROL_xxx_PATH should be absolute
@@ -44,7 +44,18 @@ DEMOPATH=$PWD/`dirname $0`
 # AAAA HAL binder
   AAAA_SOCK=unix:/var/tmp/afb-ws/alsacore
 
-# Lua compiler (On Unbutu name is lua53)
+# Basic config test
+  if test ! -S $WS_BINDIND_PATH/control; then
+    echo "ERROR: no AAAA service API to import"
+    exit 1
+  else
+    # Create an alias for AAAA controller API
+    ln -f  $WS_BINDIND_PATH/control  $WS_BINDIND_PATH/aaaa
+    AAAA_SOCK=unix:$WS_BINDIND_PATH/aaaa
+  fi
+  
+
+# # Lua compiler (On Unbutu name is lua53)
   LUAC=lua5.3
 
 # Audio-Demo local path
@@ -52,27 +63,25 @@ DEMOPATH=$PWD/`dirname $0`
   export CONTROL_LUA_PATH=$DEMOPATH/conf.d/project/lua.d
   export AFB_BINDER_NAME=AAAAdemo
 
-# include Monitoring
- if test -d $HOME/opt/afb-monitoring; then
-   AFB_MONITORING="--alias=/monitoring:$HOME/opt/afb-monitoring"
- fi 
-
 # Fulup OpenSuse Home Config
-  if [ $HOSTNAME = fulup-desktop ]; then
-    RUN_FROM_DEV_TREE=true
-    RUN_IN_GROUP=true
+  if [ $USERNAME = fulup ] ; then
+    echo "** Fulup Development Config Selected"
+    RUN_FROM_DEV_TREE=1
+    RUN_IN_GROUP=1
+    RUN_AAAA_BG=0
 
-    AFB_DEBUG_OPTION="--tracereq=common --token= --verbose"
+    # AFB_DEBUG_OPTION="--tracereq=common --token= --verbose"
 
     CTL_HOMEDEV=$HOME/Workspace/AGL-AppFW/afb-controller
     MPDC_HOMEDEV=$HOME/Workspace/AGL-AppFW/afb-mpdc
-    AAAA_HOMEDEV=$HOME/Workspace/AGL-AppFW/afb-aaaa
   fi
+
 
 # Seb's Ubuntu Config
   if [ $HOSTNAME = seb-laptop ]; then
-    RUN_FROM_DEV_TREE=true
-    RUN_IN_GROUP=true
+    RUN_FROM_DEV_TREE=1
+    RUN_AAAA_BG=1
+    RUN_IN_GROUP=1
 
     AFB_DEBUG_OPTION="--tracereq=common --token= --verbose"
 
@@ -82,24 +91,23 @@ DEMOPATH=$PWD/`dirname $0`
 
     # Ubuntu 16.4 need alsa-1.1.4 manual installation
     export LD_LIBRARY_PATH=/opt/AGL/lib/x86_64-linux-gnu:/opt/AGL/lib
-
   fi
 
 # Default installation path
   INSTALL_PREFIX=$HOME/opt
-  CTL_INSTALL=$INSTALL_PREFIX/audio-bindings
+  CTL_INSTALL=$INSTALL_PREFIX/afb-controller
   MPDC_INSTALL=$INSTALL_PREFIX/afb-mpdc
 
 # Do not try to connect on default MPD at init time
 export MPDC_NODEF_CONNECT=1
 
 # Select Dev or Installed binding
-if [ $RUN_FROM_DEV_TREE = true ]; then
-    CTL_BINDING=$CTL_HOMEDEV/build/afb-source/afb-control-afb.so
-    MPDC_BINDING=$MPDC_HOMEDEV/build/afb-mpdclient/afb-mpdc-api.so
+if [ $RUN_FROM_DEV_TREE -eq 1 ] ; then
+    CTL_BINDING=$CTL_HOMEDEV/build/ctl-binding/afb-controller.so
+    MPDC_BINDING=$MPDC_HOMEDEV/build/mpdc-binding/afb-mpdc-api.so
 else
-    CTL_BINDING=$CTL_INSTALL/afb-control-afb.so
-    MPDC_BINDING=$CTL_INSTALL/afb-mpdc-api.so
+    CTL_BINDING=$CTL_INSTALL/lib/afb-controller.so
+    MPDC_BINDING=$MPDC_INSTALL/lib/afb-mpdc-api.so
 fi
 
 
@@ -110,43 +118,40 @@ if test $? -ne 0; then
    exit
 fi
 
-echo -------
-
 # Compile lua script before starting the demo
 for FILE in  $CONTROL_LUA_PATH/*.lua; do
     $LUAC $FILE -o /dev/null
     if test $? -ne 0; then
         echo " ERROR: Must fixe LUA error before starting your binder"
-        echo -------
         exit
     fi
 done
 
+if test ! -d $WS_BINDIND_PATH; then
+   echo "  -- ERROR: AFB Websocket should exist. Request:$WS_BINDIND_PATH"
+   exit
+fi
 
-if [ $RUN_IN_GROUP = true ]; then
-    AFB_DAEMON_AAAA_CMD="afb-daemon --port=$AAAA_PORT --ldpaths=$AAAA_HOMEDEV/build --workdir=$AAAA_HOMEDEV/build \
-    --roothttp=$AAAA_HOMEDEV/htdocs --ws-server=$AAAA_SOCK $AFB_DEBUG_OPTION "
 
-    AFB_DAEMON_DEMO_CMD="afb-daemon $AFB_MONITORING --port=$DEMO_PORT  --ldpaths=/dev/null --binding=$CTL_BINDING --binding=$MPDC_BINDING \
-    --ws-client=$AAAA_SOCK --workdir=$DEMOPATH --roothttp=./htdocs $AFB_DEBUG_OPTION"
+if [ $RUN_IN_GROUP -eq 1 ] ; then
+    AFB_DAEMON_DEMO_CMD="afb-daemon --port=$DEMO_PORT  --ldpaths=/dev/null --binding=$CTL_BINDING --binding=$MPDC_BINDING \
+    --workdir=$DEMOPATH --roothttp=./htdocs --ws-client=$AAAA_SOCK  $AFB_DEBUG_OPTION "
 else
-
-    if test ! -d $WS_BINDIND_PATH; then
-        echo "  -- ERROR: AFB Websocket should exist. Request:$WS_BINDIND_PATH"
-        exit
-    fi
 
     AFB_DAEMON_DEMO_CMD="afb-daemon $AFB_MONITORING --port=$DEMO_PORT  --ldpaths=/dev/null --binding=$CTL_BINDING \
     --workdir=$DEMOPATH --roothttp=./htdocs --ws-client=$MPDC_SOCK $AFB_DEBUG_OPTION"
 fi
 
 echo ------------------------------------------------------------
-echo "$AFB_DAEMON_AAAA_CMD"
-echo ""
-exec $AFB_DAEMON_AAAA_CMD &
-AAAA_pid=$!
-sleep 1     ;# wait that binder has been started
-echo ""
+
+if [ $RUN_AAAA_BG -eq 1 ] ; then 
+    echo "$AFB_DAEMON_AAAA_CMD"
+    echo ""
+    exec $AFB_DAEMON_AAAA_CMD &
+    AAAA_pid=$!
+    sleep 1     ;# wait that binder has been started
+    echo ""
+fi
 
 echo ------------------------------------------------------------
 echo "$AFB_DAEMON_DEMO_CMD"
